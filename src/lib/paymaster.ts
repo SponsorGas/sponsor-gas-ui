97e6db78c5de26822deb05275d160ea4ca1f11b7
Karandeep Singh <karandeepsinghcaj@outlook.com>
2023-08-21 13:14:59 -0400

    removed dependency on sponsor-gas-sdk

--- src/lib/paymaster.ts
+++ src/lib/paymaster.ts
@@ -1,5 +1,5 @@
-import { SponsorGas } from "sponsor-gas-simple-sdk";
 import prisma from "./prisma";
+import { Web3Storage } from "./../../node_modules/web3.storage";
 import { Paymaster } from "sponsor-gas-simple-sdk/dist/model";
 
 export async function getUserPaymasters( userEmail:string){
@@ -23,8 +23,30 @@ export async function getUserPaymasters( userEmail:string){
 
 
 
-export async function getPaymasterById(id: string) {
-  const sponsorGas = new SponsorGas()
-  const paymaster: Paymaster | null = await sponsorGas.getPaymaster(id)
+export async function getPaymasterById(paymasterId: string) {
+  const paymaster: Paymaster | null = await prisma.paymaster.findFirst({
+    where: {
+      id: paymasterId
+    },
+    include:{PaymasterCriteria:true}
+  });
+  try{
+    const client = new Web3Storage({ token: process.env.WEB3_STORAGE_API_KEY! })
+    if(paymaster && paymaster.image){
+    
+        const imageFileResponse = await client.get(paymaster.image as string)
+        if(imageFileResponse){
+            const files = await imageFileResponse.files();
+            const imageFile = await files[0];
+            const imageURL = `https://${imageFile.cid}.ipfs.w3s.link`
+            paymaster.image = imageURL
+        }
+      }
+    } catch(e){
+        console.log('caught error')
+        console.error(e)
+        if(paymaster)
+          paymaster.image = undefined
+    }
   return paymaster
 }
